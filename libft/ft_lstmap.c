ab6be181c154db6ded0bba3b8820d72f304e1bac
Paulo Joaquim Bernardes Da Cunha <pabernar@c1r6s4.42porto.com>
2023-10-11 13:14:07 +0100

    finished lstmap

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,38 +6,51 @@
 /*   By: pabernar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 16:22:04 by pabernar          #+#    #+#             */
-/*   Updated: 2023/10/10 17:45:30 by pabernar         ###   ########.fr       */
+/*   Updated: 2023/10/11 13:04:45 by pabernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+t_list	*ft_start(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*newlist;
-	t_list	*start;
+	t_list	*newlst;
+	void	*cont;
 
 	if (!lst || !f || !del)
 		return (0);
-	newlist = 0;
+	cont = f(lst->content);
+	newlst = ft_lstnew(cont);
+	if (!newlst)
+	{
+		del(cont);
+		return (0);
+	}
+	return (newlst);
+}
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*start;
+	t_list	*newlst;
+	void	*cont;
+
+	start = ft_start(lst, f, del);
+	if (!start)
+		return (0);
+	newlst = start;
+	lst = lst->next;
 	while (lst)
 	{
-		if (!newlist)
+		cont = f(lst->content);
+		newlst->next = ft_lstnew(cont);
+		if (!newlst->next)
 		{
-			newlist = ft_lstnew(f(lst->content));
-			start = newlist;
-		}
-		else
-		{
-			newlist->next = ft_lstnew(f(lst->content));
-			newlist = newlist->next;
-		}
-		if (!newlist)
-		{
+			del(cont);
 			ft_lstclear(&start, del);
-			start = 0;
-			break;
+			return (0);
 		}
+		newlst = newlst->next;
 		lst = lst->next;
 	}
 	return (start);
