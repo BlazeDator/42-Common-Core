60bc5b89a9029b6932364f104bfce6af5128b700
Paulo Joaquim Bernardes Da Cunha <pabernar@c1r6s2.42porto.com>
2023-10-16 15:02:19 +0100

    started printf

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,22 +6,38 @@
 /*   By: pabernar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 11:28:19 by pabernar          #+#    #+#             */
-/*   Updated: 2023/10/12 12:26:24 by pabernar         ###   ########.fr       */
+/*   Updated: 2023/10/16 14:54:23 by pabernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
 // %p unsigned long / %x %X unsignd int
+// int i = va_arg(args, int); converte o valor atual em int e grava na variavel i
 
 int	ft_printf(const char *str, ...)
 {
 	va_list args;
+	int	i;
+	int	count;
 
+	i = 0;
+	count = 0;
 	va_start(args, str);
-	// int i = va_arg(args, int); converte o valor atual em int e grava na variavel i
-
-
+	while (str[i])
+	{
+		if (str[i++] == '%')
+			count += ft_typefinder(str[i++], args);
+		else
+			write(1, &str[i], 1);
+		i++;
+	}
 	va_end(args);
-	return (0); //number of characters printed
+	return (count); //number of characters printed
+}
+
+int	main(void)
+{
+	ft_printf("%i", 2147483647);
+	return (0);
 }
--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: pabernar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 11:21:26 by pabernar          #+#    #+#             */
-/*   Updated: 2023/10/12 11:28:07 by pabernar         ###   ########.fr       */
+/*   Updated: 2023/10/16 14:53:03 by pabernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,6 @@
 # include <stdarg.h>
 
 int	ft_printf(const char *str, ...);
+int	ft_typefinder(char c, va_list);
 
 #endif
