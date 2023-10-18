dcd920781f2c03606b6cf24accddb98c8dbb8f0a
Paulo Joaquim Bernardes Da Cunha <pabernar@c1r5s1.42porto.com>
2023-10-18 11:42:52 +0100

    made printf fully functional

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,38 +6,54 @@
 /*   By: pabernar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 11:28:19 by pabernar          #+#    #+#             */
-/*   Updated: 2023/10/16 14:54:23 by pabernar         ###   ########.fr       */
+/*   Updated: 2023/10/18 11:37:37 by pabernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-// %p unsigned long / %x %X unsignd int
-// int i = va_arg(args, int); converte o valor atual em int e grava na variavel i
-
 int	ft_printf(const char *str, ...)
 {
-	va_list args;
-	int	i;
-	int	count;
+	va_list	args;
+	int		i;
+	int		count;
 
 	i = 0;
 	count = 0;
 	va_start(args, str);
 	while (str[i])
 	{
-		if (str[i++] == '%')
-			count += ft_typefinder(str[i++], args);
+		if (str[i] == '%')
+		{
+			i++;
+			count += ft_typefinder(str[i], args);
+		}
 		else
+		{
 			write(1, &str[i], 1);
+			count++;
+		}
 		i++;
 	}
 	va_end(args);
-	return (count); //number of characters printed
+	return (count); 
 }
-
+/*
 int	main(void)
 {
-	ft_printf("%i", 2147483647);
+	int	count;
+
+	count = ft_printf("%c, %%testes %s", '1', "12345");
+	ft_printf("\ncount of last sentence: %i\n", count);
+	ft_printf("\t count: %i\n", ft_printf("NULL %s NULL", 0));
+	ft_printf("\t count: %i\n", ft_printf(" %p %p ", 0, 0));
+	ft_printf("\t count: %i\n", ft_printf("%i", 2147483647));
+	ft_printf("\t\t count: %d\n", ft_printf("%i", -101));
+	ft_printf("\t count: %i\n", ft_printf("%i", -2147483648));
+	ft_printf("\t count: %i\n", ft_printf("%u", 4294967295));
+	ft_printf("\t\t count: %i\n", ft_printf("%x", 255));
+	ft_printf("\t count: %i\n", ft_printf("%X", 2147483647));
+	ft_printf("\t count: %i\n", ft_printf("%p", &"ola"));
+	ft_printf("\t\t count: %i\n", ft_printf("%p", 0));
 	return (0);
-}
+}*/
