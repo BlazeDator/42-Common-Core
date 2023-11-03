95c3bd70c9f366a25a452e675b60471f29afae0d
Paulo Joaquim Bernardes Da Cunha <pabernar@c1r6s2.42porto.com>
2023-11-03 16:07:50 +0000

    Created a functioning makefile with printf and libft

---
+++ minitalk/libs/ft_printf/ft_printf.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pabernar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/12 11:21:26 by pabernar          #+#    #+#             */
+/*   Updated: 2023/10/18 11:35:50 by pabernar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdarg.h>
+
+int	ft_printf(const char *str, ...);
+int	ft_typefinder(char c, va_list args);
+int	ft_putchar(char c);
+int	ft_putstr(char *str);
+int	ft_putnbr(int n);
+int	ft_putnbr_unsigned(unsigned int n);
+int	ft_puthex(unsigned int n, int upper);
+int	ft_putpointer(unsigned long n);
+int	ft_putstr(char *str);
+
+#endif
---
+++ minitalk/minitalk.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pabernar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/03 12:42:01 by pabernar          #+#    #+#             */
+/*   Updated: 2023/11/03 16:04:18 by pabernar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <signal.h>
+# include "libs/ft_printf/ft_printf.h"
+# include "libs/libft/libft.h"
+
+#endif
