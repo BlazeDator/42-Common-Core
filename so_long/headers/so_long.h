2588335ef6fcc2f0fe9425843a1457d414920d50
Paulo Joaquim Bernardes Da Cunha <pabernar@c1r6s2.42porto.com>
2023-11-27 09:32:29 +0000

    able to close window with esc

--- so_long/headers/so_long.h
+++ so_long/headers/so_long.h
@@ -6,7 +6,7 @@
 /*   By: pabernar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 09:20:45 by pabernar          #+#    #+#             */
-/*   Updated: 2023/11/24 10:36:09 by pabernar         ###   ########.fr       */
+/*   Updated: 2023/11/27 09:21:58 by pabernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,17 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <math.h>
+# include <X11/keysym.h>
 # include "../minilibx-linux/mlx.h"
 # include "../libft/libft.h"
 
 # define WINDOW_W 1280
 # define WINDOW_H 720
+
+typedef struct	s_window
+{
+	void	*mlx;
+	void	*win;
+}		t_window;
 
 #endif
--- so_long/srcs/main.c
+++ so_long/srcs/main.c
@@ -6,23 +6,39 @@
 /*   By: pabernar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 09:21:52 by pabernar          #+#    #+#             */
-/*   Updated: 2023/11/24 09:23:24 by pabernar         ###   ########.fr       */
+/*   Updated: 2023/11/27 09:24:36 by pabernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/so_long.h"
 
+
+int	handle_no_event(void *window)
+{
+	if (window)
+		return (0);
+	return (0);
+}
+
+int	handle_input(int keysym, t_window *window)
+{
+	if (keysym == XK_Escape)
+		mlx_destroy_window(window->mlx, window->win);
+	return (0);
+}
+
 int	main(void)
 {
-	void	*mlx_ptr;
-	void	*win_ptr;
+	t_window	window;
 
-	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, WINDOW_W, WINDOW_H, "so_long");
-	while (1)
-		continue ;
-	mlx_destroy_window(mlx_ptr, win_ptr);
-	mlx_destroy_display(mlx_ptr);
-	free(mlx_ptr);
+	window.mlx = mlx_init();
+	window.win = mlx_new_window(window.mlx, WINDOW_W, WINDOW_H, "so_long");
+	
+	mlx_loop_hook(window.mlx, handle_no_event, &window);
+	mlx_key_hook(window.win, handle_input, &window);
+	mlx_loop(window.mlx);
+
+	mlx_destroy_display(window.mlx);
+	free(window.mlx);
 	return (0);
 }
