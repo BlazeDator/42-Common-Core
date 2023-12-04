5d3310d1295c4f0e73ef4385d4273e2224474ec4
Paulo Joaquim Bernardes Da Cunha <pabernar@c1r6s2.42porto.com>
2023-12-04 09:52:12 +0000

    added check forthe symbols

--- so_long/srcs/file_processing_extra.c
+++ so_long/srcs/file_processing_extra.c
@@ -6,13 +6,14 @@
 /*   By: pabernar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 09:21:52 by pabernar          #+#    #+#             */
-/*   Updated: 2023/11/27 10:00:01 by pabernar         ###   ########.fr       */
+/*   Updated: 2023/12/04 09:51:46 by pabernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/so_long.h"
 
 static int	ft_check_wall_line(char *str);
+static int	ft_check_symbols(char *str);
 /*
 TODO:
 	needs only 1 exit
@@ -35,8 +36,12 @@ int	ft_valid_map(char **map, int lines)
 			return (ft_printf("Error\nDifferent length lines\n"), 0);
 		if ((*map)[0] != '1' || (*map)[ft_strlen(*map) - 1] != '1')
 			return (ft_printf("Error\nLine not walled up\n"), 0);
+		if (!ft_check_symbols(*map))
+			return (ft_printf("Error\nCheck the symbols\n"), 0);
 		map++;
 	}
+	if (!ft_check_symbols(0))
+		return (0);
 	if (len == (unsigned int)lines)
 		return (ft_printf("Error\nThat's a square\n"), 0);
 	ft_debug_show_map(map - lines);
@@ -63,4 +68,33 @@ int	ft_free_map(char **map)
 		free(map[i++]);
 	free(map);
 	return (0);
+}
+
+int	ft_check_symbols(char *str)
+{
+	static int	exit;
+	static int	collectibles;
+	static int	player;
+
+	if (!str)
+	{
+		if (exit != 1 || player != 1)
+			return (ft_printf("Error\nOnly 1 player and 1 exit\n"), 0);
+		if (collectibles < 1)
+			return (ft_printf("Error\nneeds more collectibles\n"), 0);
+	}
+	while (str && *str)
+	{
+		if (*str != '1' && *str != '0' && *str != 'C' 
+			&& *str != 'E' && *str != 'P')
+			return (0);
+		if (*str == 'C')
+			collectibles++;
+		if (*str == 'E')
+			exit++;
+		if (*str == 'P')
+			player++;
+		str++;
+	}
+	return (1);
 }
