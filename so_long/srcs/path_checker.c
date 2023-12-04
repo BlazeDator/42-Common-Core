7b4c5e5871db0542ab17e56211b1188f68fa19c0
Paulo Joaquim Bernardes Da Cunha <pabernar@c1r6s2.42porto.com>
2023-12-04 12:35:39 +0000

    added the check for the traveler

--- so_long/srcs/path_checker.c
+++ so_long/srcs/path_checker.c
@@ -6,13 +6,14 @@
 /*   By: pabernar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 10:16:51 by pabernar          #+#    #+#             */
-/*   Updated: 2023/12/04 11:55:49 by pabernar         ###   ########.fr       */
+/*   Updated: 2023/12/04 12:33:47 by pabernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/so_long.h"
 
 static void	ft_traveler(char **map, int x, int y);
+static int	ft_traveler_check(char **map);
 
 int	ft_check_path(char **map, int lines, int len)
 {
@@ -26,7 +27,9 @@ int	ft_check_path(char **map, int lines, int len)
 	ft_traveler(map, start[0], start[1]);
 	ft_printf("\n\n");
 	ft_debug_show_map(map);
-	return (0);
+	if (!ft_traveler_check(map))
+		return (0);
+	return (1);
 }
 
 void	ft_player_pos(char **map, int *pos)
@@ -68,4 +71,24 @@ static void	ft_traveler(char **map, int x, int y)
 	y += 2;
 	if (map[x][y] == '0' || map[x][y] == 'C' || map [x][y] == 'E')
 		ft_traveler(map, x, y);
+}
+
+static int	ft_traveler_check(char **map)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (map[i])
+	{
+		j = 0;
+		while (map[i][j])
+		{
+			if (map[i][j] == 'P' || map[i][j] == 'C' || map[i][j] == 'E')
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
 }
