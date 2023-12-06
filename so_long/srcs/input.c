55a0adb03c43d9a35b7f141c98be25888148a98e
Paulo Joaquim Bernardes Da Cunha <pabernar@c1r6s2.42porto.com>
2023-12-06 12:58:32 +0000

    updated the player images to fit with the map

--- so_long/srcs/input.c
+++ so_long/srcs/input.c
@@ -6,7 +6,7 @@
 /*   By: pabernar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 11:10:45 by pabernar          #+#    #+#             */
-/*   Updated: 2023/12/06 12:22:35 by pabernar         ###   ########.fr       */
+/*   Updated: 2023/12/06 12:52:47 by pabernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,31 +18,54 @@ int	handle_input(int keysym, t_window *window)
 		window_close(window);
 	if (keysym == XK_w)
 	{
-		window->player_y -= 20;
+		window->player_y -= 24;
 		mlx_put_image_to_window(window->mlx, window->win, 
 			window->assets->player_up->img, 
 			window->player_x, window->player_y);
 	}
 	if (keysym == XK_s)
 	{
-		window->player_y += 20;
+		window->player_y += 24;
 		mlx_put_image_to_window(window->mlx, window->win, 
 			window->assets->player_down->img, 
 			window->player_x, window->player_y);
 	}
 	if (keysym == XK_a)
 	{
-		window->player_x -= 20;
+		window->player_x -= 24;
 		mlx_put_image_to_window(window->mlx, window->win, 
 			window->assets->player_left->img, 
 			window->player_x, window->player_y);
 	}
 	if (keysym == XK_d)
 	{
-		window->player_x += 20;
+		window->player_x += 24;
 		mlx_put_image_to_window(window->mlx, window->win, 
 			window->assets->player_right->img, 
 			window->player_x, window->player_y);
+	}
+	if (keysym == XK_f)
+	{
+		int i = 0;
+		int j = 0;
+
+		while (window->map[i])
+		{
+			j = 0;
+			while (window->map[i][j])
+			{
+				if (window->map[i][j] == '1')
+					mlx_put_image_to_window(window->mlx, window->win, 
+						window->assets->wall->img,
+						(j + 1) * 24, (i + 1) * 24);
+				if (window->map[i][j] == '0')
+					mlx_put_image_to_window(window->mlx, window->win, 
+						window->assets->ground->img,
+						(j + 1) * 24, (i + 1) * 24);
+				j++;
+			}
+			i++;
+		}
 	}
 	return (0);
 }
