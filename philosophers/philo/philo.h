1a7452416029846f50403031c57f9daf93e8e402
Paulo Joaquim Bernardes Da Cunha <pabernar@c1r13s10.42porto.com>
2023-12-19 09:54:09 +0000

    hello philo

---
+++ philosophers/philo/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pabernar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/19 09:44:53 by pabernar          #+#    #+#             */
+/*   Updated: 2023/12/19 09:53:23 by pabernar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <sys/time.h>
+# include <string.h>
+
+#endif
