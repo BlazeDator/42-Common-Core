15e36e4f3ccbe99734909b23b808893f92723347
Paulo Joaquim Bernardes Da Cunha <pabernar@c1r15s11.42porto.com>
2024-01-16 11:22:23 +0000

    rewrote the diff ms function

--- philo/time.c
+++ philo/time.c
@@ -6,7 +6,7 @@
 /*   By: pabernar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/02 10:47:31 by pabernar          #+#    #+#             */
-/*   Updated: 2024/01/15 11:23:56 by pabernar         ###   ########.fr       */
+/*   Updated: 2024/01/16 11:19:31 by pabernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@ static int	ft_abs(int num);
 
 int	ft_diff_ms(t_timeval *first, t_timeval *second)
 {
-	int	sum;
+	int	seconds;
+	int	microseconds;
 
-	sum = 0;
-	sum += (first->tv_sec - second->tv_sec) * 1000;
-	sum += (first->tv_usec - second->tv_usec) / 1000;
-	return (ft_abs(sum));
+	seconds = (first->tv_sec - second->tv_sec) * 1000;
+	microseconds = (first->tv_usec - second->tv_usec) / 1000;
+	return (ft_abs(seconds + microseconds));
 }
 
 static int	ft_abs(int num)
