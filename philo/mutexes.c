f68f0842a80641a913109780b0c0928977724b4f
Paulo Joaquim Bernardes Da Cunha <pabernar@c1r15s11.42porto.com>
2024-01-16 12:03:30 +0000

    fixes on mutexes.c

--- philo/mutexes.c
+++ philo/mutexes.c
@@ -6,7 +6,7 @@
 /*   By: pabernar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 15:16:50 by pabernar          #+#    #+#             */
-/*   Updated: 2024/01/16 09:57:40 by pabernar         ###   ########.fr       */
+/*   Updated: 2024/01/16 11:38:52 by pabernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,13 @@ int	ft_init_mutexes(t_philo *philos, t_info *info)
 	int	i;
 
 	i = 0;
-	if (pthread_mutex_init(&t_info.threads_mutex, 0)
-		|| pthread_mutex_init(&t_info.time_mutex, 0)
-		|| pthread_mutex_init(&t_info.stage_mutex, 0))
+	if (pthread_mutex_init(&info->threads_mutex, 0)
+		|| pthread_mutex_init(&info->time_mutex, 0)
+		|| pthread_mutex_init(&info->stage_mutex, 0))
 		return (0);
 	while (i < info->total_philos)
 	{
-		if (pthread_mutex_init(&philos[i].left_fork, 0)
-			|| pthread_mutex_init(&philos[i].stage_mutex, 0))
+		if (pthread_mutex_init(&philos[i].left_fork, 0))
 			return (0);
 		i++;
 	}
@@ -36,9 +35,10 @@ void	ft_destroy_mutexes(t_philo *philos, t_info *info)
 	int	i;
 
 	i = 0;
-	pthread_mutex_destroy(&t_info.threads_mutex, 0);
-	pthread_mutex_destroy(&t_info.time_mutex, 0);
-	while (i < info->tota_philos)
+	pthread_mutex_destroy(&info->threads_mutex);
+	pthread_mutex_destroy(&info->time_mutex);
+	pthread_mutex_destroy(&info->stage_mutex);
+	while (i < info->total_philos)
 	{
 		pthread_mutex_destroy(&philos[i].left_fork);
 		i++;
