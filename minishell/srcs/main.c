d49a396462abba796e1ca237f118db09d2fc584b
Paulo Joaquim Bernardes Da Cunha <pabernar@c1r15s12.42porto.com>
2024-01-22 14:17:06 +0000

    testing of display of shell

--- minishell/srcs/main.c
+++ minishell/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: pabernar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 11:48:43 by pabernar          #+#    #+#             */
-/*   Updated: 2024/01/22 12:43:58 by pabernar         ###   ########.fr       */
+/*   Updated: 2024/01/22 14:09:28 by pabernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,16 @@ int	main(void)
 {
 	char	dir[256];
 	char	*str;
+	char	**array;
 
 	getcwd(dir, 256);
+	array = ft_split(dir, '/');
+	while (*(array + 1))
+		array++;
+	*array = ft_strjoin("MINISHELL [/", *array);
+	*array = ft_strjoin(*array, "]>");
 	while (1)
-		str = readline(dir);
-	ft_printf("Hello %s\n", str);
+		str = readline(*array);
 	free(str);
 	return (0);
 }
