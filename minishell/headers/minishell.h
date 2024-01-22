fb8bfa2f7a2a6f0370d2092d32a53f8c5cd0af9f
Paulo Joaquim Bernardes Da Cunha <pabernar@c1r15s12.42porto.com>
2024-01-22 12:46:15 +0000

    initial test of readline

--- minishell/headers/minishell.h
+++ minishell/headers/minishell.h
@@ -6,7 +6,7 @@
 /*   By: pabernar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 12:06:26 by pabernar          #+#    #+#             */
-/*   Updated: 2024/01/22 12:07:59 by pabernar         ###   ########.fr       */
+/*   Updated: 2024/01/22 12:38:05 by pabernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,13 @@
 # define MINISHELL_H
 
 # include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <signal.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
 # include "../libft/libft.h"
 
 #endif
--- minishell/srcs/main.c
+++ minishell/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: pabernar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 11:48:43 by pabernar          #+#    #+#             */
-/*   Updated: 2024/01/22 12:09:06 by pabernar         ###   ########.fr       */
+/*   Updated: 2024/01/22 12:43:58 by pabernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,13 @@
 
 int	main(void)
 {
-	ft_printf("Hello world!\n");
+	char	dir[256];
+	char	*str;
+
+	getcwd(dir, 256);
+	while (1)
+		str = readline(dir);
+	ft_printf("Hello %s\n", str);
+	free(str);
 	return (0);
 }
