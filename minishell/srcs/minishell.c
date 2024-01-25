610acde876638477210f8e528530d658fefb0423
Paulo Joaquim Bernardes Da Cunha <pabernar@c1r15s12.42porto.com>
2024-01-25 10:54:14 +0000

    added unclosed quotes detection and minishell loop

--- minishell/srcs/minishell.c
+++ minishell/srcs/minishell.c
@@ -1,21 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pabernar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/22 11:48:43 by pabernar          #+#    #+#             */
-/*   Updated: 2024/01/25 10:45:43 by pabernar         ###   ########.fr       */
+/*   Created: 2024/01/25 09:24:42 by pabernar          #+#    #+#             */
+/*   Updated: 2024/01/25 10:53:09 by pabernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/minishell.h"
 
-int	main(int argc, char **argv)
+void	ft_minishell(void)
 {
-	if (argc != 1 && argv)
-		return (printf("No arguments\n"));
-	ft_minishell();
-	return (0);
+	char		*line;
+
+	while (1)
+	{
+		line = readline(PROMPT);
+		if (!line && printf("\n"))
+			continue ;
+		add_history(line);
+		ft_parser(line);
+		free(line);
+	}
 }
--- minishell/srcs/parser.c
+++ minishell/srcs/parser.c
@@ -1,21 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pabernar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/22 11:48:43 by pabernar          #+#    #+#             */
-/*   Updated: 2024/01/25 10:45:43 by pabernar         ###   ########.fr       */
+/*   Created: 2024/01/25 09:37:04 by pabernar          #+#    #+#             */
+/*   Updated: 2024/01/25 10:53:31 by pabernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/minishell.h"
 
-int	main(int argc, char **argv)
+int	ft_check_open_quotes(char *line);
+
+void	ft_parser(char *line)
 {
-	if (argc != 1 && argv)
-		return (printf("No arguments\n"));
-	ft_minishell();
-	return (0);
+	if (!ft_check_open_quotes(line))
+		return ;
+}
+
+int	ft_check_open_quotes(char *line)
+{
+	char	quote;
+	int		state;
+
+	state = 0;
+	while (*line)
+	{
+		if (!state && (*line == '\'' || *line == '\"'))
+		{
+			state = 1;
+			quote = *line;
+		}
+		else if (state && *line == quote)
+			state = 0;
+		line++;
+	}
+	if (state && write(2, "Error: Open quotes\n", 19))
+		return (0);
+	return (1);
 }
